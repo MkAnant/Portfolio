2c4b5ee3f2cfa878ed797333aa3ee42cdb21d62b
AnantMk <anantmk98@gmail.com>
2024-07-26 19:51:08 +0530

    Initial template for React Project

---
+++ README.md
@@ -0,0 +1,1 @@
+Building my portfolio from scratch using REACT !
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import "./variables.css"
+
+function App() {
+  return <>
+  <Header />
+  <Hero />
+  </>
+}
+
+export default App;
